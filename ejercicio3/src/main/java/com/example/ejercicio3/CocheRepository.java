bdd5c5908c1aa24dd14c6e914c94b618e3e4d2f9
Tomi <tominola99@gmail.com>
2023-01-06 12:42:15 -0300

    ejercicio 3 del curso

---
+++ ejercicio3/src/main/java/com/example/ejercicio3/CocheRepository.java
@@ -0,0 +1,13 @@
+package com.example.ejercicio3;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CocheRepository extends JpaRepository<Coche, Long> {
+    //pasar los tipos de objetos a almacenar y el tipo de la clave primaria
+
+
+
+
+}
